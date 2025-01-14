9b1ca3f9119718f668b405890e3910ba2bf31e6e
ZubkoSerhiy <serhiy_zubko@knu.ua>
2025-01-14 08:58:35 +0200

    Initial commit

--- LR6/LabLibrary/obj/Debug/net6.0/LabLibrary.AssemblyInfo.cs
+++ LR6/LabLibrary/obj/Debug/net6.0/LabLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b0f68c864c92926371507a23e6893347ab5c8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e925675ca7ebb1180b31617e389e7490a582bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
